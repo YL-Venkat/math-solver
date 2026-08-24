e4153ffd3437ee38f44f21c085c4de9774941602
YL-Venkat <lakshmivenkatyalavarthi@gmail.com>
2026-08-25 00:29:39 +0530

    Inject global logo favicon via supabase.js

--- supabase.js
+++ supabase.js
@@ -1,3 +1,5 @@
+document.head.insertAdjacentHTML('beforeend', '<link rel="icon" type="image/png" href="/log.png">');
+
 const SUPABASE_URL = "https://xdnfwxkwioobxexfcspo.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbmZ3eGt3aW9vYnhleGZjc3BvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4NzczMzIsImV4cCI6MjEwMjQ1MzMzMn0.uInuxaHkktIGEXBsl5SEyO_QaJu32-q4CC_tOrXAths";
